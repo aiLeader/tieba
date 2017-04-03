7e2453db7dd79c5730557b70b1b301df8c91985b
joyceshenhui <1426855747@qq.com>
2017-04-03 14:51:50 +0800

    index.js实现了左边贴吧分类的显示

--- tieba/src/main/webapp/js/index.js
+++ tieba/src/main/webapp/js/index.js
@@ -1,1 +1,11 @@
 UE.getEditor('topcontent');
+
+//异步加载左边板块信息
+function listType(url){
+	$.post(url,function(data){
+		for (var i = 0; i < data.rows.length; i++) {
+			$("#ulstyle").append("<li><a href='#'>"+data.rows[i].tname+"</a></li>");
+		}
+	},"json");
+}
+listType("types");
