110648376c887d388498be0b32f11560e8f66b99
joyceshenhui <1426855747@qq.com>
2017-04-03 17:11:25 +0800

    去掉首页ueditor

--- tieba/src/main/webapp/js/index.js
+++ tieba/src/main/webapp/js/index.js
@@ -1,10 +1,8 @@
-UE.getEditor('topcontent');
-
 //异步加载左边板块信息
 function listType(url){
 	$.post(url,function(data){
 		for (var i = 0; i < data.rows.length; i++) {
-			$("#ulstyle").append("<li><a href='#'>"+data.rows[i].tname+"</a></li>");
+			$("#ulstyle").append("<li><a href='page/typeDetail.jsp'>"+data.rows[i].tname+"</a></li>");
 		}
 	},"json");
 }
