0aa60f661c74870f8facfd5fe741637b27cb932c
Sinares <enterls_mail@qq.com>
2017-04-03 10:04:06 +0800

    Signed-off-by: Sinares <enterls_mail@qq.com>

--- tieba/src/main/java/com/yc/tieba/mapper/CommentsMapper.java
+++ tieba/src/main/java/com/yc/tieba/mapper/CommentsMapper.java
@@ -1,5 +1,22 @@
 package com.yc.tieba.mapper;
 
-public interface CommentsMapper {
+import java.util.List;
 
+import com.yc.tieba.entity.Comments;
+import com.yc.tieba.entity.QueryEntity;
+
+public interface CommentsMapper {
+	List<Comments> findAllComment();
+	
+	List<Comments> findBanComm();
+
+	Comments findCommById(String cid);
+
+	List<Comments> findCommByUid(QueryEntity queryEntity);
+
+	List<Comments> findCommByNid(QueryEntity queryEntity);
+
+	int banComm(String cid);
+	
+	int letComm(String cid);
 }
